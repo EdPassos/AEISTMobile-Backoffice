e17ab3902b32b7f5f4cccb42c9309306ae2d148f
EdPassos <ed.passos@gmail.com>
2014-03-19 17:48:02 +0000

    Adiciona ementas, com preço e restaurante

--- app/models/Ementa.java
+++ app/models/Ementa.java
@@ -1,9 +1,15 @@
 package models;
 
-import javax.persistence.*;
+import java.util.List;
+import java.util.Locale;
 
-import play.data.validation.Constraints.*;
-import play.db.ebean.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import play.data.format.Formatters.SimpleFormatter;
+import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
 
 @Entity
 public class Ementa extends Model {
@@ -12,7 +18,10 @@ public class Ementa extends Model {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	public static Finder<Long,Ementa> find = new Finder<Long, Ementa>(
+			Long.class, Ementa.class);
+	
 	@Id
 	public Long id;
 	
@@ -24,5 +33,40 @@ public class Ementa extends Model {
 	
 	@ManyToOne
 	public Restaurante restaurante;
+	
+	public static List<Ementa> all() {
+		  return find.all();
+	}
+	
+	public static void create(Ementa ementa) {
+		ementa.save();
+	}
+	
+	public static void delete(Long id){
+		find.ref(id).delete();
+	}
+	
+	static {
+		play.data.format.Formatters.register(Ementa.class, new EmentaFormatter());
+	}
 
+	public static class EmentaFormatter extends SimpleFormatter<Ementa> {
+
+		@Override
+		public Ementa parse(String text, Locale locale) {
+			if (text == null || text.trim().length() == 0)
+				return null;
+			return Ementa.find.ref(Long.parseLong(text.trim()));
+		}
+
+		@Override
+		public String print(Ementa value, Locale locale) {
+			return (value == null || value.id == null ? "" : value.toString());
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return description;
+	}
 }
